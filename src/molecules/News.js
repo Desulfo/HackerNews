82be56777e76f363de25459c4dfe882c81e88bf4
Pawel Janiszewski <puyol_11@interia.pl>
2020-10-22 13:53:35 +0200

    Add News Component

---
+++ src/molecules/News.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function News() {
+  return (
+    <a href="#">
+      <article className="News">
+        <header className="News__header">
+          <h3>autor + karma</h3> <data>20.10.2020</data>
+        </header>
+        <h2>News</h2>
+        <h3>40000 ✩</h3>
+      </article>
+    </a>
+  );
+}
+
+export default News;
---
+++ src/organisms/NewsContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import News from '../molecules/News';
+
+function NewsContainer() {
+  return <News />;
+}
+
+export default NewsContainer;
