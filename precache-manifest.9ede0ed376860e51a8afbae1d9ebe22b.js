65369ed1a78c6016d58941e0dfc65de0c023a357
Pawel Janiszewski <puyol_11@interia.pl>
2020-11-10 12:37:07 +0100

    Updates

---
+++ precache-manifest.9ede0ed376860e51a8afbae1d9ebe22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53979f89fcaa3c34c9aec2aa564136f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29224787bfbe6c970c4e",
+    "url": "/static/css/main.c1c8c6b8.chunk.css"
+  },
+  {
+    "revision": "d3e7c9e9f0397a2fd3f0",
+    "url": "/static/js/2.2563c569.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2563c569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29224787bfbe6c970c4e",
+    "url": "/static/js/main.98a43303.chunk.js"
+  },
+  {
+    "revision": "834b6213c02f955c8942",
+    "url": "/static/js/runtime-main.86af7f10.js"
+  }
+]);
